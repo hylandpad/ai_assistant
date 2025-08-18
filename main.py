77f04ff85e176ae734dc8d33f40d990ccf7c9e0e
hylandpad <hylandpad@gmail.com>
2025-08-17 21:34:14 -0400

    added verbose logging, calc app

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import sys
+import argparse
 from dotenv import load_dotenv
-
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,16 +11,19 @@ from google import genai
 
 client = genai.Client(api_key=api_key)
 
-def test_call():
+def test_call(messages, verbose=False):
 
-    response = client.models.generate_content(model='gemini-2.0-flash-001',contents=f"{sys.argv[1]}")
+    response = client.models.generate_content(model='gemini-2.0-flash-001',contents=messages)
     
     response_text = f"{response.text}\n"
+    user_prompt = f"User prompt: {sys.argv[1]}\n"
     candidates_tokens = f"Response tokens: {response.usage_metadata.candidates_token_count}\n"
     prompt_tokens = f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
-    print(response_text + "\n" + prompt_tokens + candidates_tokens)
     
+    print(response_text)
 
+    if verbose == True:
+        print(user_prompt + prompt_tokens + candidates_tokens)
 
 def main():
     print("Hello from ai-assistant!")
@@ -27,7 +31,12 @@ def main():
         print("No query provided, exiting program")
         sys.exit(1)
     else:
-        test_call()
+        user_prompt = " ".join(sys.argv[1])
+
+        messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+        verbose = "--verbose" in sys.argv
+        
+        test_call(messages, verbose)
 
 
 if __name__ == "__main__":
