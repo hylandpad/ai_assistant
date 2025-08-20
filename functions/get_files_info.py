39cc18f24092d50118d8437d408d1918a9d9913a
hylandpad <hylandpad@gmail.com>
2025-08-19 20:43:16 -0400

    Completed file getting/checking for get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,21 +4,26 @@ def get_files_info(working_directory, directory="."):
     target_path = os.path.join(working_directory,directory)
     working_abs_path = os.path.abspath(working_directory)
     target_abs_path = os.path.abspath(target_path)
+    
+    if target_abs_path.startswith(working_abs_path) == False:
+        return print(f"Error: Cannot list '{directory}' as it is outside the permitted working directory")
 
-    if os.path.isdir(target_abs_path) == False:
-        print(f'Error: "{target_abs_path}" is not a directory')
-    elif target_abs_path.startswith(working_abs_path) == False:
-        print(f"{directory} NOT found in {working_abs_path}")
-    else:
-        print(f"{directory} found in {working_abs_path}")
+    try:
         dir_contents = os.listdir(target_abs_path)
-        for content in dir_contents:
-            full_path = os.path.join(target_abs_path, content)
-            content_list = []
-            content_list.append(content)
-            content_list.append(os.path.getsize(full_path))
-            content_list.append(os.path.isdir(full_path))
-            print(content_list)
-
-
-get_files_info("/home/hylan/workspace/github.com/hylandpad/ai_assistant/calculator","pkg")
+    except FileNotFoundError:
+        return print(f"Error: The directory '{target_abs_path}' does not exist.")
+    except PermissionError:
+        return print(f"Error: Permission denied to access '{target_abs_path}'.")
+    except Exception as e:
+        return print(f"Error: An unhandled exception occurred [{e}]")
+    
+    content_list = []
+    for content in dir_contents:
+        full_path = os.path.join(target_abs_path, content)
+        try:
+            file_size = os.path.getsize(full_path)
+            is_dir = os.path.isdir(full_path)
+            content_list.append(f"{content}: file_size={file_size} bytes, is_dir={is_dir}")
+        except Exception as e:
+            print(f"Error: An unhandled exception occurred [{e}]")
+    return print("\n".join(content_list))
